8635bdfb3876850d953b8ee28126fe96724947e1
its-yoursk <shivanshmishra.786lucknow@gmail.com>
2026-02-09 16:33:48 +0530

    Create MaxNum.py

---
+++ MaxNum.py
@@ -0,0 +1,10 @@
+def Maximum(num1, num2):
+    if num1 > num2:
+        return num1
+    else:
+        return num2 
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+
+result = Maximum(number1, number2)
+print(f"The maximum of {number1} and {number2} is: {result}")
