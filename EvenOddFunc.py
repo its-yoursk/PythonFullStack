c4efd499e63cfdb96e8fd879d03caa5e628955ad
its-yoursk <shivanshmishra.786lucknow@gmail.com>
2026-02-09 16:32:20 +0530

    evenodd
    ex5

---
+++ EvenOddFunc.py
@@ -0,0 +1,8 @@
+def EvenOdd(num):
+    if num % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+number = int(input("Enter a number: "))
+result = EvenOdd(number)
+print(f"The number {number} is: {result}")
