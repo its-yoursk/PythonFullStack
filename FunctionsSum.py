81760c3dfeaeda63b41c3099a98b1952edc44ecb
its-yoursk <shivanshmishra.786lucknow@gmail.com>
2026-02-09 16:28:38 +0530

    FunctionsSum
    ex 2

---
+++ FunctionsSum.py
@@ -0,0 +1,6 @@
+def Sum(num1, num2):
+    return num1 + num2
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+result = Sum(number1, number2)
+print(f"The sum of {number1} and {number2} is: {result}")
