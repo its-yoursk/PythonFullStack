dbf51dfaba623ebef123e3baa63835e06603b8bd
its-yoursk <shivanshmishra.786lucknow@gmail.com>
2026-02-11 16:45:15 +0530

    sets
    .add() & .remove()

---
+++ RemoveElement.py
@@ -0,0 +1,13 @@
+n=int(input("How many elements? "))
+my_sets=set()
+for i in range(n):
+    element=input("Enter element: ")
+    my_sets.add(element)
+print("Original Sets:",my_sets)
+element_to_remove=input("Enter element to remove: ")
+if element_to_remove in my_sets:
+    my_sets.remove(element_to_remove)
+    print("Updated Sets:",my_sets)
+else:
+    print("Element not found in the set.")
+    
