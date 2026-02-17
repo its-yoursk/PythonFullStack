3d3da2543677081477f65add9e3d6ae1cddfb210
its-yoursk <shivanshmishra.786lucknow@gmail.com>
2026-02-17 16:09:49 +0530

    Update CapitalOfIndia.py

--- CapitalOfIndia.py
+++ CapitalOfIndia.py
@@ -1,13 +1,13 @@
 n=int(input("How many elements? "))
-    my_dict={}
-      for i in range(n):
+my_dict={}
+for i in range(n):
           key=input("Enter key: ")
           value=input("Enter value: ")
           my_dict[key]=value
-      print(my_dict)
-      key_to_remove=input("Enter key to remove: ")
-      if key_to_remove in my_dict:
-          del my_dict[key_to_remove]
-          print("Updated Dictionary:",my_dict)
-      else:
-          print("Key not found in the dictionary.")
+print(my_dict)
+key_to_remove=input("Enter key to remove: ")
+if key_to_remove in my_dict:
+    del my_dict[key_to_remove]
+    print("Updated Dictionary:",my_dict)
+else:
+    print("Key not found in the dictionary.")
