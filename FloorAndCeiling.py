7b88e1b2795783bd3f44645e3cd9f7dadaf571e6
its-yoursk <shivanshmishra.786lucknow@gmail.com>
2026-02-17 16:29:25 +0530

    Modules

---
+++ FloorAndCeiling.py
@@ -0,0 +1,6 @@
+import math
+num=4.7
+floor_value=math.floor(num)
+ceiling_value=math.ceil(num)
+print("Floor value of", num, "is", floor_value)
+print("Ceiling value of", num, "is", ceiling_value) 
---
+++ Power.py
@@ -0,0 +1,3 @@
+import math
+result = math.pow(5, 3)
+print("The result of 5 raised to the power of 3 is:", result)
